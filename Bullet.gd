6cc963de9a025ee5620f88e468ec119ff1165b5d
jpmendesdev <jotamendes944@gmail.com>
2025-10-18 12:07:44 -0300

    Ajeitando erro de não aparecer a bala

--- Bullet.gd
+++ Bullet.gd
@@ -8,7 +8,7 @@ var bulletDamage
 
 func _physics_process(delta):
 	
-	var pathSpawnerNode = get_tree().get_root().get_node("PathSpawner")
+	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
 	for i in pathSpawnerNode.get_child_count():
 		if pathSpawnerNode.get_child(i).name == pathName:
 			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
